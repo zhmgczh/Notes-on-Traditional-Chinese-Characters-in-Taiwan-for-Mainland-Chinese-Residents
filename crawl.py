c65f05f59c796325ed9aed4995981b6937463133
蔡于飛 <54766809+zhmgczh@users.noreply.github.com>
2024-01-06 19:41:43 -0500

    Add contents.

--- crawl.py
+++ crawl.py
@@ -8,21 +8,32 @@ def get_title(url):
     for title in soup.find_all('title'):
         result+=title.get_text()
     return result
-with open('atom.pkl','rb') as inp:
-    atom=pickle.load(inp)
-    link_set=pickle.load(inp)
-sitemap=requests.get('https://www.zh-tw.top/sitemap.xml').text
-while 0<sitemap.count('<loc>'):
-    sitemap=sitemap[sitemap.index('<loc>')+len('<loc>'):]
-    link=sitemap[:sitemap.index('</loc>')]
-    if link not in link_set:
-        title=get_title(link)
-        print(title,link)
-        if 0<title.count('一簡多繁辨析之'):
-            atom.append([title,link])
-        link_set.add(link)
-atom_file=open('atom.js','w',encoding='utf-8')
-atom_file.write(str(atom))
-with open('atom.pkl','wb') as outp:
-    pickle.dump(atom,outp,pickle.HIGHEST_PROTOCOL)
-    pickle.dump(link_set,outp,pickle.HIGHEST_PROTOCOL)
+def main(mode=0):
+    if 0==mode:
+        with open('atom.pkl','rb') as inp:
+            atom=pickle.load(inp)
+            link_set=pickle.load(inp)
+    else:
+        atom=[]
+        link_set=set()
+    sitemap=requests.get('https://www.zh-tw.top/sitemap.xml').text
+    while 0<sitemap.count('<loc>'):
+        sitemap=sitemap[sitemap.index('<loc>')+len('<loc>'):]
+        link=sitemap[:sitemap.index('</loc>')]
+        if link not in link_set:
+            title=get_title(link)
+            print(title,link)
+            if 0<title.count('一簡多繁辨析之'):
+                atom.append([title,link])
+            link_set.add(link)
+    atom_file=open('atom.js','w',encoding='utf-8')
+    atom_file.write(str(atom))
+    with open('atom.pkl','wb') as outp:
+        pickle.dump(atom,outp,pickle.HIGHEST_PROTOCOL)
+        pickle.dump(link_set,outp,pickle.HIGHEST_PROTOCOL)
+if '__main__'==__name__:
+    import sys
+    if 2==len(sys.argv):
+        main(int(sys.argv[1]))
+    else:
+        main()
