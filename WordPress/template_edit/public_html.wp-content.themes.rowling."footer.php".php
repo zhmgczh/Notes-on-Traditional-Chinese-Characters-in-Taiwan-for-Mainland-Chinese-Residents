63f0f5e41c1cb87c2b71f3a7d70b63cb7fbdaad9
蔡于飛 <54766809+zhmgczh@users.noreply.github.com>
2024-05-05 12:13:53 -0400

    Create public_html.wp-content.themes.rowling."footer.php".php

---
+++ WordPress/template_edit/public_html.wp-content.themes.rowling."footer.php".php
@@ -0,0 +1,35 @@
+		</main><!-- #site-content -->
+
+		<footer class="credits">
+					
+			<div class="section-inner">
+				
+				<a href="#" class="to-the-top">
+					<div class="fa fw fa-angle-up"></div>
+					<span class="screen-reader-text"><?php _e( 'To the top', 'rowling' ); ?></span>
+				</a>
+				
+				<!--<p class="copyright">&copy;--> <?php /*echo date( 'Y' );*/ ?> <!--<a href="--><?php /*echo esc_url( home_url( '/' ) );*/ ?><!--" rel="home">--><?php /*echo wp_kses_post( get_bloginfo( 'title' ) );*/ ?><!--</a></p>-->
+				
+				<!--<p class="attribution">--><?php /*printf( __( 'Theme by %s', 'rowling' ), '<a href="https://www.andersnoren.se">Anders Nor&eacute;n</a>' );*/ ?><!--</p>-->
+
+				<p class="copyright" style="text-align:center;">© <span id="year"></span> <a href="/" rel="home">大陸居民臺灣正體字講義</a></p>
+				<script>
+					var current_year = new Date().getFullYear();
+					var year = document.getElementById('year');
+					year.innerHTML = '民國' + (current_year - 1911) + '年（西元' + current_year + '）'
+				</script>
+				<p style="font-size:13.5px;text-align:center;">
+					All rights have been waived by the editors. The website is dedicated to the public domain.<br />
+					<a href="/copyright-and-disclaimer-%e7%89%88%e6%ac%8a%e5%92%8c%e5%85%8d%e8%b2%ac%e8%81%b2%e6%98%8e/">Copyright and Disclaimer (版權和免責聲明)</a>
+				</p>
+				
+			</div><!-- .section-inner -->
+			
+		</footer><!-- .credits -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+	
+</html>
