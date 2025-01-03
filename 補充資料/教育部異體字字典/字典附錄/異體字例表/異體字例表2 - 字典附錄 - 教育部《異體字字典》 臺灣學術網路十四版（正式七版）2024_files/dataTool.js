94b0dbd48dab564634d43cfe8838d251ce95696c
蔡于飛 <54766809+zhmgczh@users.noreply.github.com>
2025-01-03 15:58:23 -0500

    Update.

---
+++ 補充資料/教育部異體字字典/字典附錄/異體字例表/異體字例表2 - 字典附錄 - 教育部《異體字字典》 臺灣學術網路十四版（正式七版）2024_files/dataTool.js
@@ -0,0 +1,275 @@
+﻿
+
+function secTime(inp, tEnd, tStard, tType)
+{
+	ti=inp;
+	if(isNaN(ti) || ti<0) ti=0;
+	if(!tStard) tStard=0;
+	if(!tEnd) tEnd=3;
+	tEnd=Math.min(tEnd,4);
+	var timeSize=[60,60,24,30], symbol=['', ':', ':', ' ',], nullTime='00 00:00:00';
+//	if(isNaN(ti) || ti<0) return (alt?'<font color=red>':"")+nullTime.substring((4-size)*3)+'</font>';
+	var str='', s=0;
+	for(; tStard<tEnd; tStard++)
+	{
+		if((s = (i==size-1?ti:ti%timeSize[tStard]))<10)	s='0'+s;
+		ti=parseInt(ti/timeSize[i]);
+		str=s+symbol[i]+str;
+	}
+	return str;
+	
+}
+Number.prototype.secTime = function (tEnd, tStard, tType) { return secTime(this, tEnd, tStard, tType); }
+
+
+//string
+
+
+String.prototype.trim = function () {
+  return this.replace(/^\s+|\s+$/g, "");
+}
+
+function cutStr(inp, key1, key2, offset)
+{
+	if(!offset) offset=0;
+	var j=inp.indexOf(key1, offset)+key1.length, k=inp.indexOf(key2, j);
+	if(j<key1.length)	return null;
+	else if(k<0)	return str=inp.substring(j);
+	else	return str=inp.substring(j, k);
+}
+
+String.prototype.cutStr = function (key1, key2, offset) {
+  return cutStr(this, key1, key2, offset);
+}
+
+
+function toHTML(s,f)
+{
+	if(!s) 	return "";
+	if(!f)
+		s=s.replace(/ /g,"&nbsp;");
+	s=s.replace(/&/g,"&amp;")
+	.replace(/</g,"&lt;")
+	.replace(/>/g,"&gt;")
+	.replace(/\r\n/g,"<br>")
+	.replace(/\"/g,"&quot;");
+	return s;
+}
+String.prototype.toHTML = function (f) {
+  return toHTML(this,f);
+}
+
+//array--------------------------------------------------------------------
+
+function sort2(a, b, rule)
+{
+	if(!rule || !rule.length) return 0;
+	for(i=0; i<rule.length; i++)
+	{
+		var p=1, k=rule[i];
+		if(k<0)
+		{
+			p=-1;
+			k=-k;
+		}
+		k--;
+		if(a.length<k-1 || b.length<k-1) continue;
+		else if(a[k]<b[k])	return -p;
+		else if(a[k]>b[k])	return p;
+		
+		
+	}
+	return 0;
+}
+
+
+
+//--------------------------------------------------------------------
+
+/*
+ * Date Format 1.2.3
+ * (c) 2007-2009 Steven Levithan <stevenlevithan.com>
+ * MIT license
+ *
+ * Includes enhancements by Scott Trenda <scott.trenda.net>
+ * and Kris Kowal <cixar.com/~kris.kowal/>
+ *
+ * Accepts a date, a mask, or a date and a mask.
+ * Returns a formatted version of the given date.
+ * The date defaults to the current date/time.
+ * The mask defaults to dateFormat.masks.default.
+ */
+
+var dateFormat = function () {
+	var	token = /d{1,4}|m{1,4}|yy(?:yy)?|([HhMsTt])\1?|[LloSZ]|"[^"]*"|'[^']*'/g,
+		timezone = /\b(?:[PMCEA][SDP]T|(?:Pacific|Mountain|Central|Eastern|Atlantic) (?:Standard|Daylight|Prevailing) Time|(?:GMT|UTC)(?:[-+]\d{4})?)\b/g,
+		timezoneClip = /[^-+\dA-Z]/g,
+		pad = function (val, len) {
+			val = String(val);
+			len = len || 2;
+			while (val.length < len) val = "0" + val;
+			return val;
+		};
+
+	// Regexes and supporting functions are cached through closure
+	return function (date, mask, utc) {
+		var dF = dateFormat;
+
+		// You can't provide utc if you skip other args (use the "UTC:" mask prefix)
+		if (arguments.length == 1 && Object.prototype.toString.call(date) == "[object String]" && !/\d/.test(date)) {
+			mask = date;
+			date = undefined;
+		}
+
+		// Passing date through Date applies Date.parse, if necessary
+		date = date ? new Date(date) : new Date;
+		if (isNaN(date)) throw SyntaxError("invalid date");
+
+		mask = String(dF.masks[mask] || mask || dF.masks["default"]);
+
+		// Allow setting the utc argument via the mask
+		if (mask.slice(0, 4) == "UTC:") {
+			mask = mask.slice(4);
+			utc = true;
+		}
+
+		var	_ = utc ? "getUTC" : "get",
+			d = date[_ + "Date"](),
+			D = date[_ + "Day"](),
+			m = date[_ + "Month"](),
+			y = date[_ + "FullYear"](),
+			H = date[_ + "Hours"](),
+			M = date[_ + "Minutes"](),
+			s = date[_ + "Seconds"](),
+			L = date[_ + "Milliseconds"](),
+			o = utc ? 0 : date.getTimezoneOffset(),
+			flags = {
+				d:    d,
+				dd:   pad(d),
+				ddd:  dF.i18n.dayNames[D],
+				dddd: dF.i18n.dayNames[D + 7],
+				m:    m + 1,
+				mm:   pad(m + 1),
+				mmm:  dF.i18n.monthNames[m],
+				mmmm: dF.i18n.monthNames[m + 12],
+				yy:   String(y).slice(2),
+				yyyy: y,
+				h:    H % 12 || 12,
+				hh:   pad(H % 12 || 12),
+				H:    H,
+				HH:   pad(H),
+				M:    M,
+				MM:   pad(M),
+				s:    s,
+				ss:   pad(s),
+				l:    pad(L, 3),
+				L:    pad(L > 99 ? Math.round(L / 10) : L),
+				t:    H < 12 ? "a"  : "p",
+				tt:   H < 12 ? "am" : "pm",
+				T:    H < 12 ? "A"  : "P",
+				TT:   H < 12 ? "AM" : "PM",
+				Z:    utc ? "UTC" : (String(date).match(timezone) || [""]).pop().replace(timezoneClip, ""),
+				o:    (o > 0 ? "-" : "+") + pad(Math.floor(Math.abs(o) / 60) * 100 + Math.abs(o) % 60, 4),
+				S:    ["th", "st", "nd", "rd"][d % 10 > 3 ? 0 : (d % 100 - d % 10 != 10) * d % 10]
+			};
+
+		return mask.replace(token, function ($0) {
+			return $0 in flags ? flags[$0] : $0.slice(1, $0.length - 1);
+		});
+	};
+}();
+
+// Some common format strings
+dateFormat.masks = {
+	"default":      "ddd mmm dd yyyy HH:MM:ss",
+	shortDate:      "m/d/yy",
+	mediumDate:     "mmm d, yyyy",
+	longDate:       "mmmm d, yyyy",
+	fullDate:       "dddd, mmmm d, yyyy",
+	shortTime:      "h:MM TT",
+	mediumTime:     "h:MM:ss TT",
+	longTime:       "h:MM:ss TT Z",
+	isoDate:        "yyyy-mm-dd",
+	isoTime:        "HH:MM:ss",
+	isoDateTime:    "yyyy-mm-dd'T'HH:MM:ss",
+	isoUtcDateTime: "UTC:yyyy-mm-dd'T'HH:MM:ss'Z'"
+};
+
+// Internationalization strings
+dateFormat.i18n = {
+	dayNames: [
+		"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat",
+		"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+	],
+	monthNames: [
+		"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+		"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+	]
+};
+
+// For convenience...
+Date.prototype.format = function (mask, utc) {
+	return dateFormat(this, mask, utc);
+};
+
+
+
+Date.prototype.format2 = function (format) {
+    var date = {
+        "M+": this.getMonth() + 1,
+        "d+": this.getDate(),
+        "h+": this.getHours(),
+        "m+": this.getMinutes(),
+        "s+": this.getSeconds(),
+        "q+": Math.floor((this.getMonth() + 3) / 3),
+        "S+": this.getMilliseconds()
+    };
+     
+    if (/(y+)/i.test(format)) {
+        format = format.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+    }
+ 
+    for (var k in date) {
+        if (new RegExp("(" + k + ")").test(format)) {
+          format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? date[k] : ("00" + date[k]).substr(("" + date[k]).length));
+        }
+    }
+     
+    return format;
+}
+
+
+//Date.pattern
+Date.prototype.pattern=function(fmt) {
+    var o = {
+    "M+" : this.getMonth()+1, //月份
+    "d+" : this.getDate(), //日
+    "h+" : this.getHours()%12 == 0 ? 12 : this.getHours()%12, //小时
+    "H+" : this.getHours(), //小时
+    "m+" : this.getMinutes(), //分
+    "s+" : this.getSeconds(), //秒
+    "q+" : Math.floor((this.getMonth()+3)/3), //季度
+    "S" : this.getMilliseconds() //毫秒
+    };
+    var week = {
+    "0" : "\u65e5",
+    "1" : "\u4e00",
+    "2" : "\u4e8c",
+    "3" : "\u4e09",
+    "4" : "\u56db",
+    "5" : "\u4e94",
+    "6" : "\u516d"
+    };
+    if(/(y+)/.test(fmt)){
+        fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+    }
+    if(/(E+)/.test(fmt)){
+        fmt=fmt.replace(RegExp.$1, ((RegExp.$1.length>1) ? (RegExp.$1.length>2 ? "\u661f\u671f" : "\u5468") : "")+week[this.getDay()+""]);
+    }
+    for(var k in o){
+        if(new RegExp("("+ k +")").test(fmt)){
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+        }
+    }
+    return fmt;
+}
---
+++ 補充資料/教育部異體字字典/字典附錄/異體字例表/異體字例表4 - 字典附錄 - 教育部《異體字字典》 臺灣學術網路十四版（正式七版）2024_files/saveData.js
@@ -0,0 +1,42 @@
+﻿_.saveData = (function () {
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    return function (data, fileName) {
+        var json = JSON.stringify(data),
+            blob = new Blob([json], {type: "octet/stream"}),
+            url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
+    };
+}());
+
+_.txtFile = function(txt, name, opts) {
+//    var URL = window.URL || window.webkitURL
+    var a = document.createElement('a')
+    name = name || 'download.txt'
+
+    a.download = name
+    a.rel = 'noopener' // tabnabbing
+
+    // TODO: detect chrome extensions & packaged apps
+    // a.target = '_blank'
+	opts=opts || {type:"text:plain"};
+    {
+      // Support blobs
+      a.href = URL.createObjectURL(new Blob(['\ufeff', txt], opts));
+      setTimeout(function () { URL.revokeObjectURL(a.href) }, 4E4) // 40s
+      a.click();
+    }
+  }
+
+
+/*
+var data = { x: 42, s: "hello, world", d: new Date() },
+    fileName = "my-download.json";
+
+_.saveData(data, fileName);
+
+*/
