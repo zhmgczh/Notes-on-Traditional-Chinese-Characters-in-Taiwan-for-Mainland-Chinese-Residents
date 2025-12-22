aa26c7d9dd741a5814878d5bd85ef0f93a15e152
蔡于飛 <54766809+zhmgczh@users.noreply.github.com>
2025-12-22 00:41:13 -0500

    Update.

--- Dictionaries/get_renewed_dictionary_entries.py
+++ Dictionaries/get_renewed_dictionary_entries.py
@@ -32,21 +32,20 @@ def deep_merge_key(d1, d2, added_value=""):
 
 
 headers = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
-    "Accept-Language": "en,zh-TW;q=0.9,zh;q=0.8",
-    "Cache-Control": "max-age=0",
-    "Connection": "keep-alive",
-    #'Cookie':'_gid=GA1.3.1294110943.1710470589; _ga_E4NV6LGJEQ=GS1.1.1710794575.8.1.1710795175.0.0.0; _ga_8RPBR9G06C=GS1.1.1710794576.8.1.1710795175.0.0.0; _ga=GA1.1.1155386258.1710285071; _ga_S4K4QEWN8T=GS1.1.1710795305.17.1.1710795328.0.0.0; _ga_TSW7HBKV2D=GS1.1.1710795305.17.1.1710795328.0.0.0; _ga_B185ZR663X=GS1.1.1710795305.17.1.1710795328.0.0.0; JSESSIONID=DA5A04E70CC3BAB7B39090B39C2BBB2B; _ga_DY7KJGF3R2=GS1.1.1710801701.3.1.1710801872.0.0.0; _ga_P8X29BZ1XP=GS1.1.1710801701.3.1.1710801872.0.0.0',
-    "Host": "dict.variants.moe.edu.tw",
-    "Sec-Ch-Ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
-    "Sec-Ch-Ua-Mobile": "?0",
-    "Sec-Ch-Ua-Platform": '"macOS"',
-    "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "same-origin",
-    "Upgrade-Insecure-Requests": "1",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+    "connection": "keep-alive",
+    # "cookie": "_ga_7PH3J7XXS9=GS1.1.1732683926.5.1.1732684069.0.0.0; _ga_S4K4QEWN8T=GS1.1.1733352758.133.1.1733353023.0.0.0; _ga_TSW7HBKV2D=GS1.1.1733352758.436.1.1733353023.0.0.0; _ga_B185ZR663X=GS1.1.1733352758.436.1.1733353023.0.0.0; _ga=GA1.1.374967889.1716787206; _ga_DY7KJGF3R2=GS1.1.1733712566.31.1.1733712759.0.0.0; _ga_P8X29BZ1XP=GS1.1.1733712566.37.1.1733712759.0.0.0; _ga_WWRP40JZGX=GS2.1.s1752553139$o1$g0$t1752553141$j58$l0$h0; JSESSIONID=FFA6423713AAC260B11340749C0A57FD",
+    "host": "dict.variants.moe.edu.tw",
+    "sec-ch-ua": '"Google Chrome";v="143", "Chromium";v="143", "Not A(Brand";v="24"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "macOS",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
 }
 session = requests.Session()
 entries = {}
@@ -77,7 +76,7 @@ def get_id_from_response(response):
     if -1 == normal_tag:
         id = ""
     else:
-        id = html[:normal_tag].split('href="dictView.jsp?ID=')[-1].split('&')[0]
+        id = html[:normal_tag].split('href="dictView.jsp?ID=')[-1].split("&")[0]
     if "" == id:
         url_tag = html.find("location.href='dictView.jsp?ID=")
         if -1 != url_tag:
@@ -87,8 +86,8 @@ def get_id_from_response(response):
     return id
 
 
-def get_id(character, force_found=False):
-    global session, entries
+def get_id(key, character, force_found=False, max_retries=10):
+    global session, entries, manually_added_dictionary_entries
     url = "https://dict.variants.moe.edu.tw/search.jsp?QTP=0&WORD=" + quote(character)
     print(
         "Getting ID of",
@@ -101,7 +100,8 @@ def get_id(character, force_found=False):
     )
     sys.stdout.flush()
     successful = False
-    while not successful:
+    times = 0
+    while not successful and times < max_retries:
         try:
             response = session.get(url, headers=headers)
             id = get_id_from_response(response)
@@ -109,10 +109,18 @@ def get_id(character, force_found=False):
             if force_found and "" == id:
                 successful = False
                 restart_session()
+                times += 1
         except:
             restart_session()
+            times += 1
     if "" != id:
         entries[character] = id
+        write_entries()
+    elif (
+        key in manually_added_dictionary_entries
+        and character in manually_added_dictionary_entries[key]
+    ):
+        entries[character] = manually_added_dictionary_entries[key][character]
         write_entries()
     print(id)
     sys.stdout.flush()
@@ -120,7 +128,7 @@ def get_id(character, force_found=False):
 
 
 def main():
-    global entries
+    global entries, manually_added_dictionary_entries
     load_entries()
     with open("final_table_normalized.json") as final_table:
         final_json = json.load(final_table)
@@ -131,7 +139,7 @@ def main():
     ) as manually_added_dictionary_entries:
         manually_added_dictionary_entries = json.load(manually_added_dictionary_entries)
         final_json = deep_merge(final_json, manually_added_dictionary_entries)
-        
+
         stroke_json = deep_merge_key(stroke_json, manually_added_dictionary_entries, "")
     for key in final_json:
         for sub_key in final_json[key]:
@@ -139,7 +147,7 @@ def main():
                 final_json[key][sub_key] = entries[sub_key]
             else:
                 final_json[key][sub_key] = get_id(
-                    sub_key, "" != final_json[key][sub_key]
+                    key, sub_key, "" != final_json[key][sub_key]
                 )
     final = dict(
         sorted(
